9193b33731138eacb079f587b0ddd2a2e1b3e0b8
oott123 <git@public.oott123.com>
2018-04-29 21:41:13 +0800

    update proxy

--- src/firefox/onRequest.js
+++ src/firefox/onRequest.js
@@ -4,12 +4,21 @@ let debug = window.debug('pproxy:prcs:onrequest')
 
 export function onRequest(requestInfo) {
   debug('gotting request', requestInfo)
-  const useProxy = shouldUseProxy(requestInfo.url)
+  const useProxy = shouldUseProxyByRequest(requestInfo)
   if (useProxy) {
-    debug('using socks proxy')
+    debug('using proxy  (COST)')
     return { type: 'socks', host: '127.0.0.1', port: '23001' }
   } else {
-    debug('using direct')
+    debug('using direct (COST)')
     return { type: 'direct' }
   }
+}
+
+function shouldUseProxyByRequest(requestInfo) {
+  if (requestInfo.documentUrl) {
+    if (!shouldUseProxy(requestInfo.documentUrl)) {
+      return false
+    }
+  }
+  return shouldUseProxy(requestInfo.url)
 }
