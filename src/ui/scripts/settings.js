63acc19c755d0bbc0a7f6e2a4b52ea2f761396e8
oott123 <git@public.oott123.com>
2018-08-05 14:11:28 +0800

    move and remove

--- src/ui/scripts/settings.js
+++ src/ui/scripts/settings.js
@@ -7,7 +7,7 @@ async function init() {
     el: '#app',
     data: {
       ui: {
-        currentTab: 'proxies',
+        currentTab: 'scenes',
         tabs: {
           scenes: '情景模式',
           rulesets: '规则组',
@@ -17,7 +17,35 @@ async function init() {
       },
       ...state
     },
-    methods: {},
+    methods: {
+      moveUp(name, index) {
+        return move(this[name], index, -1)
+      },
+      moveDown(name, index) {
+        return move(this[name], index, 1)
+      },
+      remove(name, index) {
+        return this[name].splice(index, 1)[0]
+      },
+      moveUpRuleset() {
+        const v = this.moveUp('rulesets', this.ui.currentRulesetIndex)
+        this.ui.currentRulesetIndex = this.rulesets.indexOf(v)
+      },
+      moveDownRuleset() {
+        const v = this.moveDown('rulesets', this.ui.currentRulesetIndex)
+        this.ui.currentRulesetIndex = this.rulesets.indexOf(v)
+      },
+      removeRuleset() {
+        if (this.ui.currentRulesetIndex === this.rulesets.length - 1) {
+          alert('别删了，留一个吧，全删了有 bug 懒得修')
+          return
+        }
+        this.remove('rulesets', this.ui.currentRulesetIndex)
+        if (this.ui.currentRulesetIndex > this.rulesets.length - 1) {
+          this.ui.currentRulesetIndex = 0
+        }
+      }
+    },
     computed: {
       uiCurrentRuleset() {
         return this.rulesets[this.ui.currentRulesetIndex]
@@ -29,6 +57,12 @@ async function init() {
     vm.$watch(key, function() {}, { deep: true })
   })
   window.vm = vm
+}
+
+function move(arr, key, step) {
+  const value = arr.splice(key, 1)[0]
+  arr.splice(key + step, 0, value)
+  return value
 }
 
 init().catch(console.error)
