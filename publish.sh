e1d053991d796607c6a533b77e9481ed904aeca8
oott123 <git@public.oott123.com>
2018-08-06 11:30:16 +0800

    chore: release notes

--- publish.sh
+++ publish.sh
@@ -3,6 +3,12 @@ set -xe
 node bintray.js
 CURRENT_VERSION=`cat manifest.json | jq -r '.version'`
 CURRENT_FILENAME="proxy_proxy-${CURRENT_VERSION}-an+fx.xpi"
+RN_PATH="release-notes/$CURRENT_VERSION"
+if [ -f release-notes/$CURRENT_VERSION ] then
+  hub release create "v${CURRENT_VERSION}" -a web-ext-artifacts/$CURRENT_FILENAME -F $RN_PATH
+else
+  hub release create "v${CURRENT_VERSION}" -a web-ext-artifacts/$CURRENT_FILENAME -m ""
+fi
 jfrog bt u web-ext-artifacts/$CURRENT_FILENAME oott123/proxy-proxy/xpi/publish /xpi/
 # publish version on upload so these files are always accessable
 jfrog bt u --override --publish web-ext-artifacts/$CURRENT_FILENAME oott123/proxy-proxy/xpi/publish /xpi/proxy_proxy-latest-an+fx.xpi
