f56bc1ac37c754a40d67b6db921a7666fd881b22
oott123 <git@public.oott123.com>
2018-08-06 15:30:45 +0800

    chore: publish scripts

--- publish.sh
+++ publish.sh
@@ -3,9 +3,9 @@ set -xe
 node bintray.js
 CURRENT_VERSION=`cat manifest.json | jq -r '.version'`
 CURRENT_FILENAME="proxy_proxy-${CURRENT_VERSION}-an+fx.xpi"
-RN_PATH="release-notes/$CURRENT_VERSION"
-if [ -f release-notes/$CURRENT_VERSION ]; then
-  hub release create "v${CURRENT_VERSION}" -a web-ext-artifacts/$CURRENT_FILENAME -F $RN_PATH
+RN_PATH="release-notes/$CURRENT_VERSION.md"
+if [ -f $RN_PATH ]; then
+  hub release create "v${CURRENT_VERSION}" -a web-ext-artifacts/$CURRENT_FILENAME --file="$RN_PATH"
 else
   hub release create "v${CURRENT_VERSION}" -a web-ext-artifacts/$CURRENT_FILENAME -m "v${CURRENT_VERSION}"
 fi
